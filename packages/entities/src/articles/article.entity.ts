1d39ff11e8d93cc73beca5b563ce2e86fc4c8d0a
aiofc-nx <0774543002@163.com>
2024-12-11 13:03:05 +0800

    feat(packages): created @aiofc/entities

---
+++ packages/entities/src/articles/article.entity.ts
@@ -0,0 +1,41 @@
+import { TrackedTypeormBaseEntity } from '@aiofc/typeorm';
+import {
+  IsNumberLocalized,
+  IsStringCombinedLocalized,
+  IsUUIDLocalized,
+} from '@aiofc/validation';
+import { Expose } from 'class-transformer';
+import { Entity, PrimaryGeneratedColumn, Column, VersionColumn } from 'typeorm';
+
+@Entity('articles')
+export class Article extends TrackedTypeormBaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  @Expose()
+  @IsUUIDLocalized()
+  id!: string;
+
+  @Column({ type: String, nullable: false, length: 256 })
+  @Expose()
+  @IsStringCombinedLocalized({ minLength: 1, maxLength: 256 })
+  title!: string;
+
+  @Column()
+  author: string | undefined;
+
+  @Column({ type: String, length: 1024, nullable: false })
+  @Expose()
+  @IsStringCombinedLocalized({
+    minLength: 1,
+    maxLength: 1024,
+  })
+  description!: string;
+
+  /**
+   * 版本号
+   * 用于实现乐观锁控制
+   */
+  @VersionColumn()
+  @IsNumberLocalized()
+  @Expose()
+  version!: number;
+}
